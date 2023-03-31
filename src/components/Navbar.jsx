7f669aa7fd0781015762761cde1d323e1596b14f
dileo24 <joacodileo@gmail.com>
2023-03-31 13:54:03 -0300

    searchbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "../img/logo.png";
+import SearchBar from "./SearchBar";
 
 export default function Navbar() {
   return (
     <div className="nav-container">
       <img src={logo} alt="logo" />
+      <SearchBar />
     </div>
   );
 }
