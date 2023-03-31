eea78ed7612aa303eea0eab2df1a1cf97f6f62a2
cesarzv <cesarzegarrav26@gmail.com>
2023-03-31 15:06:08 -0500

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div className="container">
-      <h2>Home</h2>
+      <h2>Homee</h2>
       <div className="prueba"></div>
     </div>
   );
