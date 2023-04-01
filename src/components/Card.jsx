785b69d9558f9392af919b63b75b37114f6665ab
dileo24 <joacodileo@gmail.com>
2023-03-31 21:33:11 -0300

    arreglo cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,18 +6,26 @@ export default function Card({ nombre, categoria, subcategoria }) {
   return (
     <div className="card">
       <div className="info">
-        <p className="nombre">
-          <b>Producto: </b>
-          {nombre}
-        </p>
-        <p className="categoria">
-          <b>Categoría: </b>
-          {categoria}
-        </p>
-        <p className="subcategoria">
-          <b>Subcategoría: </b>
-          {subcategoria}
-        </p>
+        <div className="cadaInfo">
+          <p className="nombre">
+            <b>Producto: </b>
+            {nombre}
+          </p>
+        </div>
+
+        <div className="cadaInfo">
+          <p className="categoria">
+            <b>Categoría: </b>
+            {categoria}
+          </p>
+        </div>
+
+        <div className="cadaInfo">
+          <p className="subcategoria">
+            <b>Subcategoría: </b>
+            {subcategoria}
+          </p>
+        </div>
       </div>
       <div className="imagenes">
         <img src={editar} alt="editar" />
