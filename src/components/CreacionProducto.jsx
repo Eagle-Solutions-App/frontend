b25e769cf5086124e548388d345f2eba243d4234
dileo24 <joacodileo@gmail.com>
2023-04-01 17:52:54 -0300

    creación del prod inicio

--- src/components/CreacionProducto.jsx
+++ src/components/CreacionProducto.jsx
@@ -5,6 +5,7 @@ import Navbar from "./Navbar";
 
 export default function CreacionProducto() {
   const categs = useSelector((state) => state.categorias);
+  const subCategs = useSelector((state) => state.subCategorias);
   const [input, setInput] = useState({
     id: "",
     nombre: "",
@@ -13,33 +14,48 @@ export default function CreacionProducto() {
     descripcion: "",
   });
 
-  const [nombreC, setNombreC] = useState("");
+  const [nombreCateg, setNombreCateg] = useState("");
+  const [nombreSubC, setNombreSubC] = useState("");
   return (
     <div>
       <Navbar />
       <div className="container">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <button>Home</button>
+          <button>Inicio</button>
         </Link>
         <h2>Creación de Producto</h2>
 
         {/* creacion de categoría */}
         <form>
           <div>
-            <label>New Category: </label>
-            <input type="text" name="nombre" value={nombreC}></input>
+            <label>Nueva Categoría: </label>
+            <input type="text" name="nombre" value={nombreCateg}></input>
 
             <div>
-              <button type="submit" disabled={!nombreC}>
-                Create category!
+              <button type="submit" disabled={!nombreCateg}>
+                Crear categoría!
               </button>
             </div>
           </div>
         </form>
 
-        {/* creacion de producto */}
+        {/* creación de subcategoría */}
         <form>
-          <p>Select one category!</p>
+          <div>
+            <label>Nueva Subcategoría: </label>
+            <input type="text" name="nombre" value={nombreSubC}></input>
+
+            <div>
+              <button type="submit" disabled={!nombreSubC}>
+                Crear Subcategoría!
+              </button>
+            </div>
+          </div>
+        </form>
+
+        {/* selección de categoría */}
+        <form>
+          <p>Selecciona una categoría!</p>
           <div>
             {categs?.map((obj) => {
               return (
@@ -61,13 +77,40 @@ export default function CreacionProducto() {
             })}
           </div>
 
+          {/* selección de subcategoría */}
+          <p>Selecciona una Subcategoría!</p>
           <div>
-            <label>Title: </label>
+            {subCategs?.map((obj) => {
+              return (
+                <div key={obj.id}>
+                  <label htmlFor={obj.nombre} key={obj.id}>
+                    {obj.nombre}
+                    <div>
+                      <input
+                        type="checkbox"
+                        name="subCateg"
+                        id={obj.id}
+                        value={obj.nombre}
+                      />
+                      <button value={obj.id}>x</button>
+                    </div>
+                  </label>
+                </div>
+              );
+            })}
+          </div>
+
+          {/* creación del producto */}
+          <div>
+            <label>Nombre del producto: </label>
+            <input type="text" name="title" value={input.title}></input>
+            <br></br>
+            <label>Código (ej: #3524): </label>
             <input type="text" name="title" value={input.title}></input>
           </div>
 
           <div>
-            <label>Content: </label>
+            <label>Descripción: </label>
             <textarea
               type="text"
               name="content"
