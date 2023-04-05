428d18b1fc22b541363c5f1acb309737babaf5ca
cesarzv <cesarzegarrav26@gmail.com>
2023-04-05 15:30:10 -0500

    landing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CreacionProducto from "./components/CreacionProducto";
 import Usuarios from "./components/Usuarios";
 import Depositos from "./components/Depositos";
 import EditarProducto from "./components/EditarProducto";
-/* import Landing from "./components/Landing"; */
+import Landing from "./components/Landing";
 import { Routes, Route } from "react-router-dom";
 import axios from "axios";
 import CreacionDeposito from "./components/CreacionDeposito";
@@ -15,8 +15,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route exact path="/" element={<Landing />} /> */}
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/producto" element={<CreacionProducto />} />
         <Route path="/editarProd/:id" element={<EditarProducto />} />
         <Route path="/createDepositos" element={<CreacionDeposito />} />
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div className="landCont">
+      <div className="landingContainer">
+        <div className="wrapper">
+          <h1>Eagle Solutions</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+            consectetur animi, quia magni null.
+          </p>
+          <NavLink to="/home">
+            <button>Ingresar</button>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
