8c2588c0d4bed88ff24966a2fad8d89f587c8f1c
dileo24 <joacodileo@gmail.com>
2023-04-11 01:25:49 -0300

    error al crear user e ir al panel

--- src/components/Creacion/CreacionUsuario.jsx
+++ src/components/Creacion/CreacionUsuario.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { postUser } from "../../redux/actions/actions";
+import { getRoles, postUser } from "../../redux/actions/actions";
 import Navbar from "../Navbar";
 
 export default function CreacionDeposito() {
@@ -9,26 +9,27 @@ export default function CreacionDeposito() {
   const dispatch = useDispatch();
   const rol = useSelector((state) => state.roles);
 
-  const numeroAleatorio = Math.floor(Math.random() * Math.pow(10, 5))
-    .toString()
-    .padStart(5, "0");
-
   const [input, setInput] = useState({
     nombre: "",
     apellido: "",
     email: "",
-    rol: "",
-    clave: numeroAleatorio,
+    rolID: "",
+    empresaID: 1,
   });
+
+  useEffect(() => {
+    dispatch(getRoles());
+  }, [dispatch]);
 
   const handlerChange = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
 
   const handlerSelectRol = (e) => {
-    if (!input.rol.includes(e.target.value)) {
-      setInput({ ...input, rol: e.target.value });
+    if (!input.rolID.includes(e.target.value)) {
+      setInput({ ...input, rolID: e.target.value });
     }
+    console.log(e.target.value);
   };
 
   const handlerSubmitForm = (e) => {
@@ -40,7 +41,8 @@ export default function CreacionDeposito() {
       nombre: "",
       apellido: "",
       email: "",
-      rol: "",
+      rolID: "",
+      empresaID: 1,
     });
     navigate("/usuarios");
   };
@@ -84,12 +86,12 @@ export default function CreacionDeposito() {
                 ></input>
               </div>
 
-              <div>
-                <select onSubmit={(e) => handlerSelectRol(e)}>
-                  {rol?.map((obj, i) => {
+              <div className="selectCat">
+                <select onChange={(e) => handlerSelectRol(e)}>
+                  {rol?.map((obj) => {
                     return (
-                      <option value={obj} key={i}>
-                        {obj}
+                      <option value={obj.id} key={obj.id}>
+                        {obj.rol}
                       </option>
                     );
                   })}
