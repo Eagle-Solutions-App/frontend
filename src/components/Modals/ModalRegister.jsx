01f4a2e92c978894ee2f912604b5b10569edb881
Joaquin <joacodileo@gmail.com>
2023-04-15 23:09:10 -0300

    modal register terminado

--- src/components/Modals/ModalRegister.jsx
+++ src/components/Modals/ModalRegister.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../img/logo.png"
+import logo from "../../img/logo.png";
 
 export default function ModalRegister({ onClose }) {
   const [username, setUsername] = useState("");
@@ -19,14 +19,15 @@ export default function ModalRegister({ onClose }) {
         </span>
         <h2>Registrarse</h2>
         <div className="info">
-          <div>
+          <div className="form">
             <form onSubmit={handleSubmit}>
               <label>
-                Nombre de usuario:
+                Nombre de empresa:
                 <input
                   type="text"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
+                  autoFocus
                 />
               </label>
               <label>
@@ -54,10 +55,26 @@ export default function ModalRegister({ onClose }) {
             </form>
           </div>
           <div className="logo">
+            <p>
+              Nuestra plataforma permitirá tener un control en tiempo real del
+              stock de materiales general y particular de cada obra, gestión de
+              pedidos, inventarios, sincronización de stock, control de
+              empleados, etc.
+            </p>
             <img src={logo}></img>
+          </div>
+        </div>
+        <div className="suscripciones">
+          <div>
+            <h3>Suscripción Mensual</h3>
+            <h1>$$$</h1>
+          </div>
+          <div>
+            <h3>Suscripción Anual</h3>
+            <h1>$$$</h1>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
